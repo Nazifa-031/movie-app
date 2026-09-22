820a02e98b0b4aab712a31d465f68936bf6d70a7
Nazifa <nazifash31@gmail.com>
2026-09-22 18:24:44 +0400

    built movie card and  api logic !

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,84 @@
 import { allMovies } from "./services/tmdb";
-
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MovieCards from "./components/MovieCards";
+import Loader from "./components/Loader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [movies, setmovies] = useState([]); // [moviesarray, setmoviesarray]
+
+  const Movies = async () => {
+    setIsLoading(true);
+    setError("");
+    try {
+      const response = await allMovies();
+      console.log(response); // {page: 1, results: Array(20), total_pages: 1001, total_results: 20001}
+
+      const movies = response.results;
+      setmovies(movies);
+      console.log(movies);  // {page: 1, results: Array(20), total_pages: 1001, total_results: 20001}
+    } catch (error) {
+      setError(error);
+      console.error(`Error in the Movies Function: ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
-    allMovies()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    console.log(API_KEY);
+    Movies();
   }, []);
-  return <div>API Test</div>;
+
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
+    <p>{error}</p>
+  ) : (
+    <ul>
+      {movies.map((movie) => (
+        <MovieCards key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
 };
 
 export default App;
+
+// response obj
+// has result of (20) arrays of objects
+//
+// adult
+// :
+// false
+
+// genre_ids
+// :
+// (3) [878, 28, 12]
+// id
+// :
+// 969681
+// original_language
+// :
+// "en"
+
+// overview
+// :
+// "Fighting crime full-time as Spider-Man in a world that doesn't remember him—and the pressure of seeing his old friends move on without him—sparks a change in Peter Parker he may not have the power to control. But that transformation might also be the only thing that can stop a shocking new threat to the city and those he loves - a powerful villain no one can even see."
+// popularity
+// :
+// 704.4472
+// poster_path
+// :
+// "/bjiS5ipwxb9JFy3XRRN4OAilSeX.jpg"
+// release_date
+// :
+// "2026-07-29"
+
+// title
+// :
+// "Spider-Man: Brand New Day"
+
+// vote_average
+// :
+// 7.862
---
+++ src/components/MovieCards.jsx
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical, faLanguage } from "@fortawesome/free-solid-svg-icons";
+import noPoster from '../assets/no-poster.png';
+
+const MovieCards = ({
+  movie: {
+    title,
+    id,
+    vote_average,
+    release_date,
+    poster_path,
+    original_language,
+  },
+}) => {
+  return (
+    <div>
+      <li>
+        <img src={poster_path ? `http://image.tmdb.org/t/p/w185/${poster_path}` : `${noPoster}`} alt="poster" />
+        <FontAwesomeIcon icon={faEllipsisVertical} />
+        <h3>{title}</h3>
+        <h4>{release_date}</h4>
+        <h4>{vote_average}</h4>
+        <h4>{original_language}</h4>
+      </li>
+    </div>
+  );
+};
+
+export default MovieCards;
+
+
